a0283d8bcefcb211b61ff7117b17e3ac0c266fc9
Phan Minh Tai <taipm.vn@gmail.com>
2019-06-19 18:02:45 +0700

    Clone code from CodeSummer.club

---
+++ Web/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public const int PageSize = 10;
+        public BaseController()
+        {
+            ViewBag.Title = ConfigurationManager.AppSettings["Title"];
+            ViewBag.Logo = ConfigurationManager.AppSettings["Logo"];
+        }
+    }
+}
